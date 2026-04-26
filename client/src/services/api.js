dd504945846d41a6be30e495068551f7f63db43c
Miguel Ferreira <miguelccezarferreira@gmail.com>
2026-04-26 13:49:19 -0300

    feat: frontend preparado para produção

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,5 @@
-import axios from 'axios'
+import axios from "axios";
 
-const api = axios.create({
-  baseURL: 'http://localhost:3001/api',
-})
-
-export default api
+export const api = axios.create({
+  baseURL: import.meta.env.VITE_API_URL
+});
